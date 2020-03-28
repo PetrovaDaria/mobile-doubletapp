7050f81b5bf676e04fdb278d34e86659a5ddf286
PetrovaDaria <daartpetrova@mail.ru>
2020-03-28 10:55:12 +0500

    Секрет неработащего бокового меню раскрыт

--- app/src/main/java/com/example/habittracker/activities/MainActivity.kt
+++ app/src/main/java/com/example/habittracker/activities/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.example.habittracker.Habit
 import com.example.habittracker.R
@@ -18,70 +19,31 @@ import kotlinx.android.synthetic.main.toolbar.*
 class MainActivity : AppCompatActivity(), ListCallback, EditHabitCallback, NavigationView.OnNavigationItemSelectedListener {
     private val LIST_TAG = "ListFragment"
     private val EDIT_HABIT_TAG = "EditHabitFragment"
+    private val ABOUT_TAG = "About Fragment"
     private val viewPagerFragment = ViewPagerFragment()
-
-    // private lateinit var myToolbar: Toolbar
-    // private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // myToolbar = toolbar
-        // setSupportActionBar(toolbar)
-
-//        drawerToggle = ActionBarDrawerToggle(this, main_activity, toolbar, R.string.open_drawer, R.string.close_drawer)
-//        drawerToggle.isDrawerIndicatorEnabled = true
-//        main_activity.addDrawerListener(drawerToggle)
-//        drawerToggle.syncState()
-//        navigation_drawer.setNavigationItemSelectedListener(this)
-//        navigation_drawer.bringToFront()
-
         val navView = findViewById<NavigationView>(R.id.navigation_drawer)
         navView.setNavigationItemSelectedListener(this)
-        navView.bringToFront()
 
         if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(
-                    R.id.main_activity,
-                    viewPagerFragment,
-                    LIST_TAG
-                )
-                .addToBackStack(LIST_TAG)
-                .commit()
+            replaceFragment(viewPagerFragment, LIST_TAG)
         }
     }
 
     override fun onAddHabit() {
-        println("add")
-        supportFragmentManager
-            .beginTransaction()
-            .replace(
-                R.id.main_activity,
-                EditHabitFragment(),
-                EDIT_HABIT_TAG
-            )
-            .addToBackStack(EDIT_HABIT_TAG)
-            .commit()
+        replaceFragment(EditHabitFragment(), EDIT_HABIT_TAG)
     }
 
     override fun onEditHabit(habit: Habit, habitPosition: Int) {
-        println("edit")
-        supportFragmentManager
-            .beginTransaction()
-            .replace(
-                R.id.main_activity,
-                EditHabitFragment.newInstance(habit, habitPosition),
-                EDIT_HABIT_TAG
-            )
-            .addToBackStack(EDIT_HABIT_TAG)
-            .commit()
+        val fragment = EditHabitFragment.newInstance(habit, habitPosition)
+        replaceFragment(fragment, EDIT_HABIT_TAG)
     }
 
     override fun onSaveHabit(habit: Habit, habitPosition: Int) {
-        println("save")
         val bundle = Bundle()
         bundle.putInt("habitPosition", habitPosition)
         bundle.putParcelable("habit", habit)
@@ -90,20 +52,34 @@ class MainActivity : AppCompatActivity(), ListCallback, EditHabitCallback, Navig
     }
 
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
-        println("click")
         val fragment = selectDrawerItem(p0.itemId)
+        val tag = selectDrawerItemTag(p0.itemId)
+        replaceFragment(fragment, tag)
+        main_activity.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun replaceFragment(fragment: Fragment, tag: String) {
         supportFragmentManager
             .beginTransaction()
-            .replace(R.id.main_activity, fragment)
+            .replace(R.id.fragment_container, fragment, tag)
+            .addToBackStack(tag)
             .commit()
-        return true
     }
 
     private fun selectDrawerItem(itemId: Int): Fragment {
         return when (itemId) {
-            R.id.nav_list -> viewPagerFragment
-            R.id.nav_about -> AboutFragment()
+            R.id.menu_item_main -> viewPagerFragment
+            R.id.menu_item_about -> AboutFragment()
             else -> AboutFragment()
+        }
+    }
+
+    private fun selectDrawerItemTag(itemId: Int): String {
+        return when (itemId) {
+            R.id.menu_item_main -> LIST_TAG
+            R.id.menu_item_about -> ABOUT_TAG
+            else -> ABOUT_TAG
         }
     }
 }
